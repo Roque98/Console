e61ba718598908b586ff05c7e54e0546e16e9e99
Roque98 <ingroque98@gmail.com>
2025-12-01 22:09:44 -0600

    feat: Refactor Code Generator and add new features
    This commit refactors the Code Generator module and adds new features.
    
    Changes:
    
    - Refactored ProyectoController.
    
    - Deleted CodeGeneratorController and CodeViewerController.
    
    - Added CodeGeneratorViewerController, InputPromptController, and PromptsController.
    
    - Updated entities and repositories for the Code Generator.
    
    - Updated views for the Code Generator.

--- FolderViewWeb/Dapper/CodeGenerator/Entidades/CodeGeneratorArchivoEntidad.cs
+++ FolderViewWeb/Dapper/CodeGenerator/Entidades/CodeGeneratorArchivoEntidad.cs
@@ -7,9 +7,11 @@
         public string Extension { get; set; }
         public string Documentacion { get; set; }
         public string Contenido { get; set; }
+        public string path { get; set; }
         public int Version { get; set; }
         public DateTime FechaCreacion { get; set; }
         public int? IdArchivoPadre { get; set; }
+        public int idPromptTemplate { get; set; }
         public CodeGeneratorArchivoEntidad ArchivoPadre { get; set; }
         public ProyectoEntidad Proyecto { get; set; }
     }
---
+++ FolderViewWeb/Dapper/Repositorios/TipoProyectoRepository.cs
@@ -0,0 +1,73 @@
+﻿//using Dapper;
+//using FolderView.Dapper.Entidades;
+//using FolderView.Dapper.Interfaces;
+//using System.Data;
+
+//namespace FolderView.Dapper.Repositorios
+//{
+//    public class TipoProyectoRepository: ITipoProyectoRepository
+//    {
+//        private readonly DapperContext _context;
+//        private readonly ITipoArchivoTipoProyectoRepository _tipoArchivoTipoProyectoRepository;
+//        public TipoProyectoRepository(
+//            DapperContext context,
+//            ITipoArchivoTipoProyectoRepository tipoArchivoTipoProyectoRepository
+//        )
+//        {
+//            _context = context;
+//            _tipoArchivoTipoProyectoRepository = tipoArchivoTipoProyectoRepository;
+//        }
+
+//        public async Task<List<ReadTipoProyectoEntidad>> CreateTipoProyectoAsync(CreateTipoProyectoEntidad dto)
+//        {
+//            var query = "consolaMonitoreo..CodeGenerator_TipoProyecto_Add";
+//            var connection = _context.CreateConnection();
+//            var param = new { dto.Nombre, dto.Descripcion, dto.UrlImagen };
+//            await connection.ExecuteAsync(query, param, commandType: CommandType.StoredProcedure);
+//            return await GetAllTipoProyectoAsync();
+//        }
+
+//        public async Task<ReadTipoProyectoEntidad> GetTipoProyectoByIdAsync(int id)
+//        {
+//            var query = "consolaMonitoreo..CodeGenerator_TipoProyecto_GetById";
+//            var connection = _context.CreateConnection();
+//            var param = new { id };
+//            var resultado = await connection.QuerySingleOrDefaultAsync<ReadTipoProyectoEntidad>(query, param, commandType: CommandType.StoredProcedure);
+
+//            if (resultado != null)
+//            {
+//                resultado.TiposArchivosRelacion.AddRange(
+//                    await _tipoArchivoTipoProyectoRepository.GetAllByIdTipoProyectoTipoArchivoTipoProyectoAsync(resultado.Id)
+//                );
+//            }
+
+//            return resultado;
+//        }
+
+//        public async Task<List<ReadTipoProyectoEntidad>> GetAllTipoProyectoAsync()
+//        {
+//            var query = "consolaMonitoreo..CodeGenerator_TipoProyecto_GetAll";
+//            var connection = _context.CreateConnection();
+//            var resultado = await connection.QueryAsync<ReadTipoProyectoEntidad>(query, commandType: CommandType.StoredProcedure);
+//            return resultado.ToList();
+//        }
+
+//        public async Task<List<ReadTipoProyectoEntidad>> UpdateTipoProyectoAsync(UpdateTipoProyectoEntidad dto)
+//        {
+//            var query = "consolaMonitoreo..CodeGenerator_TipoProyecto_Update";
+//            var connection = _context.CreateConnection();
+//            var param = new { dto.Id, dto.Nombre, dto.Descripcion, dto.UrlImagen };
+//            await connection.ExecuteAsync(query, param, commandType: CommandType.StoredProcedure);
+//            return await GetAllTipoProyectoAsync();
+//        }
+
+//        public async Task<bool> DeleteTipoProyectoAsync(int id)
+//        {
+//            var query = "consolaMonitoreo..CodeGenerator_TipoProyecto_Delete";
+//            var connection = _context.CreateConnection();
+//            var param = new { id };
+//            await connection.ExecuteAsync(query, param, commandType: CommandType.StoredProcedure);
+//            return true;
+//        }
+//    }
+//}
